2f2f79585fa73f78fb5406f2e15d41eb637a6162
Alexpogo471 <56255801+Alexpogo471@users.noreply.github.com>
2019-12-23 10:09:55 +0300

    add custom header

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -2,23 +2,33 @@ import io.restassured.RestAssured;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.path.json.JsonPath;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 public class TestMain {
 
+    // RequestFilter
     public static void debug() {
         RestAssured.filters(
                 ResponseLoggingFilter.responseLogger(),
                 new RequestLoggingFilter());
     }
 
+    @BeforeClass(description = "Base URL")
+    public void setup() {
+        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+    }
+
+
     @Test
     public void testApiAlbumsTitle0(){
-        String URL = "https://jsonplaceholder.typicode.com/albums";
+        setup();
         JsonPath body = given()
-                .when().get(URL)
+                .when().get("/albums")
                 .then().statusCode(200).extract().body().jsonPath();
         debug();
         System.out.println(body.get("title[0]").toString());
@@ -26,100 +36,112 @@ public class TestMain {
 
     @Test
     public void testApiAlbumsTitle30(){
-        String URL = "https://jsonplaceholder.typicode.com/albums";
-        JsonPath body = given().log().all()
-                .when().get(URL)
-                .then().extract().body().jsonPath();
+        setup();
+        JsonPath body = (JsonPath) given()
+                .when().get("/albums?id=6")
+                .then().assertThat()
+                .body(matchesJsonSchemaInClasspath("Json.json"));
+        debug();
         System.out.println(body.get("title[30]").toString());
     }
 
     @Test
     public void testApiAlbumsId5(){
-        String URL = "https://jsonplaceholder.typicode.com/albums";
-        JsonPath body = given().log().all()
-                .when().get(URL)
+        setup();
+        JsonPath body = given()
+                .when().get("/albums")
                 .then().extract().body().jsonPath();
+        debug();
         System.out.println(body.get("id[5]").toString());
     }
 
     @Test
     public void testApiUsers1(){
-        String URL = "https://jsonplaceholder.typicode.com/users";
-        JsonPath body = given().log().all()
-                .when().get(URL)
+        setup();
+        JsonPath body = given().header("User","Agent")
+                .when().get("/users")
                 .then().extract().body().jsonPath();
+        debug();
         System.out.println(body.get("name[1]").toString());
     }
 
     @Test
     public void testApiToDos(){
-        String URL = "https://jsonplaceholder.typicode.com/todos";
-        JsonPath body = given().log().all()
-                .when().get(URL)
+        setup();
+        JsonPath body = given().header("User","Agent")
+                .when().get("/todos")
                 .then().extract().body().jsonPath();
+        debug();
         System.out.println(body.get("title[0]").toString());
     }
 
     @Test
     public void testApiToDos2(){
-        String URL = "https://jsonplaceholder.typicode.com/todos";
-        JsonPath body = given().log().all()
-                .when().get(URL)
+        setup();
+        JsonPath body = given().header("User","Agent")
+                .when().get("/todos")
                 .then().extract().body().jsonPath();
+        debug();
         System.out.println(body.get("title[3]").toString());
     }
 
     @Test
     public void testApiPhotos(){
-        String URL = "https://jsonplaceholder.typicode.com/photos";
-        JsonPath body = given().log().all()
-                .when().get(URL)
+        setup();
+        JsonPath body = given()
+                .when().get("/photos")
                 .then().extract().body().jsonPath();
+        debug();
         System.out.println(body.get("url[0]").toString());
     }
 
     @Test
     public void testApiPhotos2(){
-        String URL = "https://jsonplaceholder.typicode.com/photos";
-        JsonPath body = given().log().all()
-                .when().get(URL)
+        setup();
+        JsonPath body = given()
+                .when().get("/photos")
                 .then().extract().body().jsonPath();
+        debug();
         System.out.println(body.get("url[2]").toString());
     }
 
     @Test
     public void testApiComments(){
-        String URL = "https://jsonplaceholder.typicode.com/comments";
-        JsonPath body = given().log().all()
-                .when().get(URL)
+        setup();
+        JsonPath body = given()
+                .when().get("/comments")
                 .then().extract().body().jsonPath();
+        debug();
         System.out.println(body.get("body[0]").toString());
     }
 
     @Test
     public void testApiComments2(){
-        String URL = "https://jsonplaceholder.typicode.com/comments";
-        JsonPath body = given().log().all()
-                .when().get(URL)
+        setup();
+        JsonPath body = given()
+                .when().get("/comments")
                 .then().extract().body().jsonPath();
+        debug();
         System.out.println(body.get("body[2]").toString());
     }
 
     @Test
     public void testApiUsers2(){
-        String URL = "https://jsonplaceholder.typicode.com/users";
-        JsonPath body = given().log().all()
-                .when().get(URL)
+        setup();
+        JsonPath body = given()
+                .when().get("/users")
                 .then().extract().body().jsonPath();
+        debug();
         System.out.println(body.get("phone[2]").toString());
     }
 
     @Test
     public void testApiUsers3(){
-        String URL = "https://jsonplaceholder.typicode.com/users";
-        JsonPath body = given().log().all()
-                .when().get(URL)
+        setup();
+        JsonPath body = given()
+                .when().get("/users?name=2")
                 .then().extract().body().jsonPath();
+        debug();
         System.out.println(body.get("website[2]").toString());
     }
 }
